acf5aa6f345b6397a0fc0f4b25ff83b296b7eee1
nphau011 <nphau011@gmail.com>
2018-08-02 17:10:33 +0700

    add session6 and 8

---
+++ app/script_haunp1/handle_event.js
@@ -0,0 +1,19 @@
+var app = angular.module('HauNP1_app01', []);
+
+app.controller('handleEventController', ['$scope', function ($scope) {
+    var technologies = [
+        { Name: "C#", Likes: 0,Minus:0, Dislikes: 0 },
+        { Name: "ASP.NET", Likes: 0,Minus:0, Dislikes: 0 },
+        { Name: "SQL Server", Likes: 0, Minus: 0, Dislikes: 0 },
+        { Name: "AngularJS", Likes: 0, Minus: 0, Dislikes: 0 }
+    ];
+    $scope.technologies = technologies;
+    $scope.increaseLike = function (technology) {
+        technology.Likes++;
+        technology.Minus = technology.Likes - technology.Dislikes;
+    }
+    $scope.increaseDislike = function (technology) {
+        technology.Dislikes++;
+        technology.Minus = technology.Likes - technology.Dislikes;
+    }
+}]);
